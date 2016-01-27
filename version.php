10b8281a1df27538425aa80c2dc979de655ca35b
Sam Chaffee <sam@moodlerooms.com>
2016-01-27 14:08:46 -0700

    INT-8108: Bumped versions in OS plugins

--- version.php
+++ version.php
@@ -25,8 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015070100; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2014111000; // Requires this Moodle version.
+$plugin->version   = 2016012600; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2015051103; // Requires this Moodle version (2.9.3)
+$plugin->release   = '2.9.3 (Build: 20160204)';
 $plugin->component = 'tool_themetester'; // Full name of the plugin (used for diagnostics).
 
 $plugin->maturity  = MATURITY_STABLE; // This version's maturity level.
