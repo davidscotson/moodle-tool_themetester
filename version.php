385a1eaff15d7d091c83bac5ea7163202c17b5b8
David Scotson <david.scotson@synergy-learning.com>
2018-09-19 22:26:37 +0100

    Bump version for 3.5

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017120800; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018091800; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012051500; // Requires this Moodle version.
-$plugin->release   = '3.3.3';
+$plugin->release   = '3.5.2';
 $plugin->component = 'tool_themetester'; // Full name of the plugin (used for diagnostics).
 
 $plugin->maturity  = MATURITY_STABLE; // This version's maturity level.
